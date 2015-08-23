88f59f1b6d84dbc63520c5f1d8971985fe286d14
Bryan Stoner <bryan.stoner@idfive.com>
2015-08-23 19:35:53 -0400

    Meanie gitignore

---
+++ src/js/site.js
@@ -0,0 +1,58 @@
+/*
+
+  js/site
+    contains all javascript functions pertaining to the site
+
+*/
+
+
+// ========================================
+// Run Main Site Function
+// ========================================
+
+function site() {
+
+  // Notify the developer that the site is ready
+  console.log('mimo is running away!');
+
+  // Add a listener for debug purposes
+  window.addEventListener("keydown", debug, false);
+
+}
+
+
+// ========================================
+// Additional Debug Function
+// ========================================
+
+function debug(e) {
+
+  // g
+  if (e.keyCode == 71) {
+    if (document.body.classList.contains('debug-grid')) {
+      document.body.classList.remove('debug-grid');
+    }
+    else {
+      document.body.classList.add('debug-grid');
+    }
+  }
+  // o
+  else if (e.keyCode == 79) {
+    if (document.body.classList.contains('debug-outlines')) {
+      document.body.classList.remove('debug-outlines');
+    }
+    else {
+      document.body.classList.add('debug-outlines');
+    }
+  }
+  // r
+  else if (e.keyCode == 82) {
+    if (document.body.classList.contains('debug-ruler')) {
+      document.body.classList.remove('debug-ruler');
+    }
+    else {
+      document.body.classList.add('debug-ruler');
+    }
+  }
+
+}
