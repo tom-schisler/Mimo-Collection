a6f7c6949923562beab0e3011dca2522c8d96fe7
Bryan Stoner <mimoduo@gmail.com>
2015-08-23 22:36:18 -0400

    Update drawer mixin and js

--- src/js/site.js
+++ src/js/site.js
@@ -15,6 +15,15 @@ function site() {
   // Notify the developer that the site is ready
   console.log('mimo is running away!');
 
+  // Activate the drawer upon clicking the drawer trigger
+  var drawerTrigger = document.querySelector('.drawer-trigger');
+  var drawerTriggerClose = document.querySelector('.drawer-trigger-close');
+  var drawer = function (event) {
+    document.body.classList.toggle('drawer-active');
+  }
+  drawerTrigger.addEventListener('click', drawer, false);
+  drawerTriggerClose.addEventListener('click', drawer, false);
+
   // Add a listener for debug purposes
   window.addEventListener("keydown", debug, false);
 
